f902787a5ba5b772f552016eb6dd4f9dfd2e2989
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-15 13:46:28 +0430

    added error handling and error notification

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { Button, Skeleton, TextField } from "@radix-ui/themes"
+import { Button, Callout, Skeleton, TextField } from "@radix-ui/themes"
 import { useForm, Controller } from "react-hook-form"
 import React, { useEffect, useState } from "react"
+import { AiFillInfoCircle } from "react-icons/ai"
 import { MdEditor, Themes } from "md-editor-rt"
 import { useRouter } from "next/navigation"
 import { useTheme } from "next-themes"
@@ -15,7 +16,8 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
-  const [isClient, setIsClient] = useState(false)
+  const [isClient, setIsClient] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
 
   const { register, control, handleSubmit } = useForm<IssueForm>()
   const { resolvedTheme } = useTheme()
@@ -26,18 +28,32 @@ const NewIssuePage = () => {
   }, [])
 
   const onSubmit = async (data: IssueForm) => {
-    await axios.post("/api/issues", data)
-    router.push("/issues")
+    try {
+      await axios.post("/api/issues", data)
+      router.push("/issues")
+    } catch (e) {
+      setError("an unexpected error occurred")
+    }
   }
 
   return (
-    <form
-      className="max-w-2xl space-y-3"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <TextField.Root placeholder="Title" {...register("title")} />
-      {isClient ? (
-        <>
+    <div className="max-w-2xl space-y-3">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Icon>
+            <AiFillInfoCircle />
+          </Callout.Icon>
+          <Callout.Text>
+            {error}
+          </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        {isClient ? (
           <Controller
             render={({ field }) => {
               return (
@@ -46,19 +62,20 @@ const NewIssuePage = () => {
                   {...field}
                   theme={resolvedTheme as Themes}
                   language="en-US"
-                  modelValue={field.value ? field.value : "Add a description..."}
+                  modelValue={field.value}
+                  placeholder="Add a description..."
                 />
               )
             }}
             name="description"
             control={control}
           />
-        </>
-      ) : (
-        <Skeleton className="rounded-md" height={"500px"} />
-      )}
-      <Button>Submit New Issue</Button>
-    </form>
+        ) : (
+          <Skeleton className="rounded-md" height={"500px"} />
+        )}
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   )
 }
 
