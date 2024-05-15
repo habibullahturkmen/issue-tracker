b69f0b26123547e23cabd59868f70a9f3a3cf3f9
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-15 12:55:03 +0430

    Create Issue: Handle form submission

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,14 +1,62 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes"
-import MarkdownEditor from "@/app/components/MarkdownEditor"
+"use client"
+
+import { Button, Skeleton, TextField } from "@radix-ui/themes"
+import { useForm, Controller } from "react-hook-form"
+import React, { useEffect, useState } from "react"
+import { MdEditor, Themes } from "md-editor-rt"
+import { useRouter } from "next/navigation"
+import { useTheme } from "next-themes"
+import "md-editor-rt/lib/style.css"
+import axios from "axios"
+
+interface IssueForm {
+  title: string
+  description: string
+}
 
 const NewIssuePage = () => {
+  const [isClient, setIsClient] = useState(false)
+
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const { resolvedTheme } = useTheme()
+  const router = useRouter()
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
-    <div className="max-w-2xl space-y-3">
-      <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Description" />
-      <MarkdownEditor />
+    <form
+      className="max-w-2xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data)
+        router.push("/issues")
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      {isClient ? (
+        <>
+          <Controller
+            render={({ field }) => {
+              return (
+                <MdEditor
+                  className="rounded-md"
+                  {...field}
+                  theme={resolvedTheme as Themes}
+                  language="en-US"
+                  modelValue={field.value ? field.value : "Add a description..."}
+                />
+              )
+            }}
+            name="description"
+            control={control}
+          />
+        </>
+      ) : (
+        <Skeleton height={"500px"} />
+      )}
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
