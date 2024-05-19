149ce1708c77b8b6ce5c806adbd01657084ca527
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-19 14:25:27 +0430

    Issue details page: edit button added

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,10 +1,12 @@
-import { Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
+import { RxPencil2 } from "react-icons/rx"
 import React, { FC } from "react"
 
 import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import prisma from "../../../../prisma/client"
 import dynamic from "next/dynamic"
+import Link from "next/link"
 
 const MarkdownPreview = dynamic(() => import("@/app/components/MarkdownPreview"), { ssr: false })
 
@@ -26,16 +28,24 @@ const IssueDetailsPage: FC<IssueDetailsPageType> = async ({ params }) => {
   }
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2" align="center">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <MarkdownPreview text={issue.description} />
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="4">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3" my="2" align="center">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <MarkdownPreview text={issue.description} />
+        </Card>
+      </Box>
+      <Box>
+        <Button className="cursor-pointer">
+          <RxPencil2 />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
 
