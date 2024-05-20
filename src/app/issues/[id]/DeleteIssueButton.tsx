d47cc1eb46d907160c1968e3a57f1d11f0819a97
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-20 13:40:10 +0430

    added a confirmation dialog for the delete issue button

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,41 @@
+import { AlertDialog, Button, Flex } from "@radix-ui/themes"
+import { GoTrash } from "react-icons/go"
 import React, { FC } from "react"
-import { Button } from "@radix-ui/themes"
 
 interface DeleteIssueButtonType {
   id: number
 }
 
 const DeleteIssueButton: FC<DeleteIssueButtonType> = ({ id }) => {
-  return <Button color="red">Delete Issue</Button>
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" className="cursor-pointer w-lg">
+          <GoTrash />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue?
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray" className="cursor-pointer">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" className="cursor-pointer">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  )
 }
 
 export default DeleteIssueButton
