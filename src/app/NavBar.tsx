2132108f2051ec1028de843297b260b6ae1d4b83
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-20 21:45:10 +0430

    next-auth: login and logout links added

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { usePathname } from "next/navigation"
+import { useSession } from "next-auth/react"
 import { AiFillBug } from "react-icons/ai"
+import { Box } from "@radix-ui/themes"
 import Link from "next/link"
 import { clsx } from "clsx"
 import React from "react"
@@ -9,7 +11,9 @@ import React from "react"
 import ThemeSwitch from "@/app/components/ThemeSwitch"
 
 const NavBar = () => {
+  const { status, data: session } = useSession()
   const currentPath = usePathname()
+
   const links = [
     { id: 0, label: "Dashboard", href: "/" },
     { id: 1, label: "Issues", href: "/issues" },
@@ -41,6 +45,10 @@ const NavBar = () => {
         ))}
         <ThemeSwitch />
       </ul>
+      <Box>
+        { status === "authenticated" && <Link href="/api/auth/signout">Logout</Link>}
+        { status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
+      </Box>
     </nav>
   )
 }
--- src/app/context/providers.tsx
+++ src/app/context/providers.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from "next-themes"
+import React from "react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-      {children}
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
