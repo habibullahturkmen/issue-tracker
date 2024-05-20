31c7ff18f5ef2393e302d8e8c2a73f4e457cc469
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-20 22:13:13 +0430

    navbar layout improved

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,9 +1,9 @@
 "use client"
 
+import { Box, Container, Flex } from "@radix-ui/themes"
 import { usePathname } from "next/navigation"
 import { useSession } from "next-auth/react"
 import { AiFillBug } from "react-icons/ai"
-import { Box } from "@radix-ui/themes"
 import Link from "next/link"
 import { clsx } from "clsx"
 import React from "react"
@@ -20,35 +20,41 @@ const NavBar = () => {
   ]
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link
-        className="text-zinc-700 hover:text-black dark:text-gray-300 dark:hover:text-white"
-        href="/"
-      >
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6 items-center">
-        {links.map((link) => (
-          <li key={link.id}>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
             <Link
-              className={clsx({
-                "text-zinc-900 dark:text-white": link.href === currentPath,
-                "text-zinc-500 dark:text-gray-300 hover:text-zinc-900 dark:hover:text-white":
-                  link.href !== currentPath,
-                "transition-colors": true,
-              })}
-              href={link.href}
+              className="text-zinc-700 hover:text-black dark:text-gray-300 dark:hover:text-white"
+              href="/"
             >
-              {link.label}
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-        <ThemeSwitch />
-      </ul>
-      <Box>
-        { status === "authenticated" && <Link href="/api/auth/signout">Logout</Link>}
-        { status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
-      </Box>
+            <ul className="flex space-x-6 items-center">
+              {links.map((link) => (
+                <li key={link.id}>
+                  <Link
+                    className={clsx({
+                      "text-zinc-900 dark:text-white": link.href === currentPath,
+                      "text-zinc-500 dark:text-gray-300 hover:text-zinc-900 dark:hover:text-white":
+                        link.href !== currentPath,
+                      "transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+              <ThemeSwitch />
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && <Link href="/api/auth/signout">Logout</Link>}
+            {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   )
 }
