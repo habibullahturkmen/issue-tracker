2859ed860be247fbe0047c2fb35698235cbf2fbb
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-05-28 16:40:01 +0430

    configured react query

--- src/app/context/providers.tsx
+++ src/app/context/providers.tsx
@@ -1,15 +1,22 @@
 "use client"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from "next-themes"
 import React from "react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient()
+
   return (
-    <SessionProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-        {children}
-      </ThemeProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
