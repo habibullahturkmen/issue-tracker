d95c2ea81d8aeb47342cc4591a025610923e1f9d
habibullahturkmen <habibullah_turkmen@yahoo.com>
2024-06-02 14:53:43 +0430

    added pagination functionality

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,5 +1,8 @@
-import React, { FC } from "react"
+"use client"
+
+import { useRouter, useSearchParams } from "next/navigation"
 import { Button, Flex, Text } from "@radix-ui/themes"
+import React, { FC } from "react"
 import {
   RxChevronLeft,
   RxChevronRight,
@@ -14,9 +17,18 @@ interface PaginationType {
 }
 
 const Pagination: FC<PaginationType> = ({ itemCount, pageSize, currentPage }) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const pageCount = Math.ceil(itemCount / pageSize)
   if (pageCount <= 1) {
     return null
+  }
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set("page", page.toString())
+    router.push("?" + params.toString())
   }
 
   return (
@@ -24,16 +36,36 @@ const Pagination: FC<PaginationType> = ({ itemCount, pageSize, currentPage }) =>
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="grass" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="grass"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <RxDoubleArrowLeft />
       </Button>
-      <Button color="grass" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="grass"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <RxChevronLeft />
       </Button>
-      <Button color="grass" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="grass"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <RxChevronRight />
       </Button>
-      <Button color="grass" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="grass"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <RxDoubleArrowRight />
       </Button>
     </Flex>
